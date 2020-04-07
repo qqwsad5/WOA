26e8196b0ccac7d728a5d37aa9b429ad84df3933
LYD <liaoyd16@mails.tsinghua.edu.cn>
2020-04-07 11:36:06 +0800

    debug WebUtils.py

--- backend/backend.py
+++ backend/backend.py
@@ -18,6 +18,11 @@ if __name__ == '__main__':
 
     iters_before_find_repost = Meta.SLEEP_UPDATE // Meta.SLEEP_SEARCH
     while True:
+        iters_before_find_repost -= 1
+        if iters_before_find_repost == 0:
+            update.update_db()
+            iters_before_find_repost = Meta.SLEEP_UPDATE // Meta.SLEEP_SEARCH
+
         rumorwords_update_time = Database.read_update_time()
 
         WebUtils.rumorwords_to_weibo_list(Meta.KEYWORDS, rumorwords_update_time)
@@ -26,11 +31,6 @@ if __name__ == '__main__':
         Database.commit()
 
         time.sleep(Meta.SLEEP_SEARCH)
-        
-        iters_before_find_repost -= 1
-        if iters_before_find_repost == 0:
-            update.update_db()
-            iters_before_find_repost = Meta.SLEEP_UPDATE // Meta.SLEEP_SEARCH
 
     Meta.unload_hanlp_recognizer()
     Database.disconnect()
